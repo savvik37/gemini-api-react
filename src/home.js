2fc8e5889093bebba6fd23520379a6d301f86fb5
savvik37 <savvikopikovs@yahoo.com>
2025-01-10 06:03:16 +0000

    more styling

--- src/home.js
+++ src/home.js
@@ -1,7 +1,10 @@
 import {React, useEffect, useState} from 'react'
 import Tilt from 'react-parallax-tilt'
+import ReactMarkdown from 'react-markdown'
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import axios from 'axios';
+
+console.log(process.env)
 
 const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
@@ -14,6 +17,7 @@ export default function Home() {
     const sendMessage = async (e) => {
       try{
         e.preventDefault();
+        document.getElementById("responseBoxId").classList.add('hidden');
         const msg = prompt 
         setLoading(false)
         const res = await axios.post(`${REACT_APP_API_BASE_URL}/sendmsg`, { prompt: msg })
@@ -25,42 +29,39 @@ export default function Home() {
       }finally{
         setLoading(true)
     }
-
-
   }
 
-
-
     useEffect(()=>{
+      document.getElementById("responseBoxId").classList.add('hidden');
     }, [])
 
   return (
     <div class="mainContainer">
         <div class="heading">
-          <Tilt options={{ tiltReverse: false }}>
+          <Tilt tiltReverse={true} tiltMaxAngleX={0} >
             <h1 class="dm-ita">Gemini API Bot</h1>
           </Tilt>
         </div> 
 
       {loading ? (
 
-          <div>
-            {response}
-          </div>
+            <div id="responseBoxId" class="responseBox fade-in-response">
+              <ReactMarkdown children={response} />
+            </div>
 
         ) : (
+
             <div class="loading">
               <img src="icon2.png" alt="Loading..." />
             </div>
+
         )
       }
-      <div class="submitForm">
+      <div class="submitForm fade-in">
 
           <form onSubmit={sendMessage} class="submitFormInner">
-            <input type="text" value={prompt} onChange={({ target }) => setPrompt(target.value)} />
-
+              <input type="text" value={prompt} onChange={({ target }) => setPrompt(target.value)} placeholder="type a message"/>
               <button type="submit" class="sendMsg">Send</button>
-
           </form>
 
       </div>
